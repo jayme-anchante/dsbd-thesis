08c37607edbadfb1d17ff7b9494f4ddebb1b2667
Jayme Anchante <jayme.anchante@disroot.org>
2019-06-23 23:10:40 -0300

    changed docker image version

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-IMAGE=dsbd-thesis
+IMAGE=dsbd-thesis:0.1
 echo "compiling $1"
 docker run --rm -v $PWD:/home/ -it $IMAGE sh -c "R --quiet -e 'library(tinytex); tinytex::pdflatex(as.character(\"$1\"))'"
